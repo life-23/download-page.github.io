18b7949026575766cd1e58cd7b96bf4241acb944
life-23 <145614951+life-23@users.noreply.github.com>
2023-11-29 18:11:58 -0500

    v_1

---
+++ src/script/main.js
@@ -0,0 +1,7 @@
+function downloadWindows() {
+  window.location.href = "https://github.com/life-23/Download_LIFE/tree/792814fe245d6fa415419e11354e98965175fe82/Windows";
+}
+
+function downloadAndroid() {
+  window.location.href = "https://github.com/life-23/Download_LIFE/tree/792814fe245d6fa415419e11354e98965175fe82/Android";
+}
